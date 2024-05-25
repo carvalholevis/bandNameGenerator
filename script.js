874601d288f2389465efd1a7a51d42c1834492ad
Leviton Lima Carvalho <149610344+carvalholevis@users.noreply.github.com>
2024-05-25 11:11:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -41,24 +41,9 @@ app.get('/', (req, res) => {
 })
 
 app.post('/submit', function (req, res) {
-    res.send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <title>YOUR BAND NAME IS:...</title>
-    </head>
-    <body>
-    
-        <div class="container">
-            <h1 class="h1">Your band name is: </h1>
-            <h2 class="display-5"> ${bandName} </h2>
-        </div>
-        
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    </body>
-    </html>`)
+    res.render("submit.ejs", 
+        {name: bandName}
+    )
     // console.log(bandName)
 })
 
